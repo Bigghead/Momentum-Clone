45169461206c154a98f2cf6e87ed15c7b1b4475e
Tu An Huynh Van <arishuynhvan@gmail.com>
2016-12-16 22:59:57 +0800

    added input for To-do items & implemented the storing function

--- js/todo.js
+++ js/todo.js
@@ -2,3 +2,39 @@
   * Authors:
   *
   */
+
+  if (window.localStorage) {
+
+  	var txtToDo = document.getElementById('new-todo').value;
+
+  	
+
+  }
+
+
+  /** To store to-do items
+    * @param: string
+    */
+    function set_todos(txtToDo) {
+    	var arrToDo = [];
+    	if (localStorage.length>0) 
+    		arrToDo.join(localStorage.getItem('todoList');
+    			arrToDo.push(txtToDo);
+    			localStorage.setItem('todoList',arrToDo);
+
+    		}
+
+    /** To retrieve to-do items & prepare the DOM
+      * 
+      */
+
+      function addTodo(){
+      	var newTodoItem = document.createElement("li", );
+
+      }
+
+    	
+
+
+    	}
+
